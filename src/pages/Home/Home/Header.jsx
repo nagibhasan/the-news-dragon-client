2416a315cb7c73b5e50a5e66222accbcf2a54eb6
nagibhasan <nagibhasan132@gmail.com>
2023-05-01 02:53:52 +0600

    header nav

--- src/pages/Home/Home/Header.jsx
+++ src/pages/Home/Home/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
@@ -11,9 +12,31 @@ const Header = () => {
             <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
             <p>{moment().format("dddd, MMMM Do YYYY")}</p>
            </div>
-           <div>
-           <Button variant="danger">Latest</Button>
+           <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={100}>
+                I can be a React component, multiple React components, or just some text.... Match Highlights: Germany vs Spain — as it happened ! Match Highlights: Germany vs Spain as...
+                </Marquee>
            </div>
+           <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Carrer</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </Container>
     );
 };
