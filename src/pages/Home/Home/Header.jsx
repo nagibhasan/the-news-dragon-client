71d29096a8e5174a6e7956503cc8c94e3b01895a
nagibhasan <nagibhasan132@gmail.com>
2023-05-01 02:06:06 +0600

    header tools

--- src/pages/Home/Home/Header.jsx
+++ src/pages/Home/Home/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
+        <Container>
            <div className="text-center">
            <img src={logo} alt="" />
             <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
-            <p>{moment().format()}</p>
+            <p>{moment().format("dddd, MMMM Do YYYY")}</p>
            </div>
-        </div>
+           <div>
+           <Button variant="danger">Latest</Button>
+           </div>
+        </Container>
     );
 };
 
